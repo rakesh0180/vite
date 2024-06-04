53f55780449b02a097258a85a73d7aea2116f65e
Rakesh M <rakeshraki0180@gmail.com>
2024-06-04 08:43:25 +0530

    feat: code refactor

--- my-react-modal/src/component/Modal.jsx
+++ my-react-modal/src/component/Modal.jsx
@@ -1,11 +1,31 @@
 import "./modal.scss";
-const Modal = ({ isOpen, onClose, children }) => {
+// eslint-disable-next-line react/prop-types
+const Modal = ({
+  isOpen,
+  onClose,
+  heading = "Modal Header",
+  children,
+  footer = "Modal Footer",
+}) => {
   return (
     <div
       className={`modal-overlay ${isOpen ? "active" : "un-active"}`}
       onClick={onClose}
     >
-      {children}
+      <div className="modal-container">
+        <div className="modal-header">
+          <>
+            <h2 className="header">{heading}</h2>
+            <button className="close-btn" onClick={onClose}>
+              X
+            </button>
+          </>
+        </div>
+        <div className="modal-body">{children}</div>
+        <div className="modal-footer">
+          <p>{footer}</p>
+        </div>
+      </div>
     </div>
   );
 };
