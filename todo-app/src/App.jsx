114684067613ec0ef3735ebab55179b2a78b6248
Rakesh M <rakeshraki0180@gmail.com>
2024-08-15 23:20:26 +0530

    feat: inline edit

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import ToDoList from "./assets/component/to-do/ToDoList";
+
+function App() {
+  const data = [
+    {
+      id: 1,
+      name: "John Doe",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+    },
+    {
+      id: 3,
+      name: "Michael Johnson",
+    },
+    { id: 4, name: "Emily Davis" },
+  ];
+
+  return (
+    <>
+      <div className="container">
+        <header className="header">
+          <h1>To Do App</h1>
+        </header>
+        <main className="main">
+          <ToDoList data={data} />
+        </main>
+        <footer className="footer">
+          <h3>@copy write 2024</h3>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ todo-app/src/assets/component/to-do/ToDoList.jsx
@@ -0,0 +1,116 @@
+import { useId, useState } from "react";
+
+const ToDoList = ({ data }) => {
+  const [add, setAdd] = useState("");
+  const [todoList, setTodoList] = useState(data);
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState("");
+  const [error, setError] = useState({});
+
+  const id = useId();
+
+  const handleAdd = () => {
+    if (add) {
+      const newItem = {
+        id,
+        name: add,
+      };
+      setTodoList([...todoList, newItem]);
+      setAdd("");
+      setError({});
+    } else {
+      setError({ add: "field should not be empty" });
+    }
+  };
+
+  const handleEdit = (todo) => {
+    setEditId(todo.id);
+    setEditText(todo.name);
+  };
+
+  const handleDelete = (id) => {
+    const filterData = todoList.filter((ele) => ele.id !== id);
+    setTodoList(filterData);
+  };
+
+  const handleSave = (data) => {
+    const updateData = todoList.map((todo) => {
+      return todo.id === data.id ? { ...todo, name: editText } : todo;
+    });
+    setTodoList(updateData);
+    setEditId(null);
+    setEditText("");
+  };
+
+  const handleCancel = () => {
+    setEditId(null);
+    setEditText("");
+  };
+
+  const handleInputChange = (e) => {
+    setAdd(e.target.value);
+    setError({});
+  };
+
+  return (
+    <div className="todo-container">
+      <div className="add-section">
+        <div className="input-container">
+          <input
+            type="text"
+            name="add"
+            id=""
+            value={add}
+            onChange={(e) => {
+              handleInputChange(e);
+            }}
+          />
+          {error.add && (
+            <label htmlFor="add" className="error">
+              {error.add}
+            </label>
+          )}
+        </div>
+
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <div className="operation-section">
+        {todoList?.map((todo) => (
+          <div key={todo.id} className="name-row">
+            {editId === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  className="name"
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                />
+                <button className="save" onClick={() => handleSave(todo)}>
+                  Save
+                </button>
+                <button className="cancel" onClick={handleCancel}>
+                  Cancel
+                </button>
+              </>
+            ) : (
+              <>
+                <p className="name">{todo.name}</p>
+                <button className="edit" onClick={() => handleEdit(todo)}>
+                  Edit
+                </button>
+                <button
+                  className="delete"
+                  onClick={() => handleDelete(todo.id)}
+                >
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ToDoList;
